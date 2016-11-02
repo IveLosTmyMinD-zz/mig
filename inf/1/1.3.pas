c9f5d4344c4c93be41a0bf47ce66510194af1410
IveLosTmyMinD <ivelostmymind8@gmail.com>
2016-11-02 12:19:36 +0400

    Create 1.3.pas

---
+++ inf/1/1.3.pas
@@ -0,0 +1,12 @@
+//1.3
+var a, b, c, r:real;
+begin
+readln(a);
+readln(b);
+readln(c);
+if (a-b)*exp(a-b)+c+1 <> 0 then //Исключение нуля в знаменателе
+r:=((a+b)/(abs(a)+abs(b))+(1+a-b)/(1+abs(a)-abs(b)))/((a-b)*exp(a-b)+c+1)//Решение
+else
+writeln('her');//Если ноль в знаменателе, то выведет это
+writeln(r);
+end.
