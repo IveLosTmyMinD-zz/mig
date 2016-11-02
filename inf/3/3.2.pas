4dfae7e54a0f2e0cb05bf21f8ecade9358a5802b
IveLosTmyMinD <ivelostmymind8@gmail.com>
2016-11-02 12:24:03 +0400

    Update 3.2.pas

--- inf/3/3.2.pas
+++ inf/3/3.2.pas
@@ -1,1 +1,21 @@
-
+var a, b, p, x:real;
+begin
+readln(a);
+readln(b);
+if a > 0 then
+  begin
+    p:=-b/a;
+    writeln('x>',p);
+  end
+else
+  if a < 0 then
+    begin
+      p:=-b/a;
+       writeln('x<', p);
+    end
+  else
+    if b > 0 then
+      writeln('Решением является любое число')
+    else
+      writeln('Неравенство не имеет решений');
+end.
