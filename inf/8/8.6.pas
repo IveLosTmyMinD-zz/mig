82f01b7f08a815bca94a1cea660e6ed12409745d
IveLosTmyMinD <ivelostmymind8@gmail.com>
2016-11-08 21:53:37 +0300

    Create 8.6.pas

---
+++ inf/8/8.6.pas
@@ -0,0 +1,18 @@
+var x,c,z,a,s:real;
+i,n:integer;
+begin
+readln(x);
+readln(n);
+c:=x;
+z:=1;
+a:=c/z;
+s:=a;
+for i:=1 to n do
+	begin
+		c:=c*c;
+		z:=z*(z+1);
+		a:=c/z;
+		s:=a;
+	end;
+writeln(s);
+end.
