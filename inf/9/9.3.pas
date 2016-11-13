6430eda91329d01cb3a2855fdd997b1dddaf51fe
IveLosTmyMinD <ivelostmymind8@gmail.com>
2016-11-13 18:06:05 +0300

    Create 9.3.pas

---
+++ inf/9/9.3.pas
@@ -0,0 +1,16 @@
+var a:array [1..10] of integer;
+i, n, k, s:integer;
+begin
+s:=1;
+  for i:=1 to 10 do
+    begin
+      readln(a[i]);
+       if a[i] > 0 then
+          begin
+            k:=k+1;
+            s:=s*a[i];
+          end;
+    end;
+writeln('Количество положительных элементов массива: ', k);
+writeln('Произведение положительных элементов массива: ',s);
+end.
