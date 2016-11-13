13fc7ec54c04b78c6b91d9818dc46cb6cd9c9b38
IveLosTmyMinD <ivelostmymind8@gmail.com>
2016-11-13 19:05:45 +0300

    Update 1.1.pas

--- inf/1/1.1.pas
+++ inf/1/1.1.pas
@@ -1,7 +1,9 @@
 //1.1
 var s, t, v:real;   //Объявляем переменные
 begin               //Начало программы
+writeln('Vvedite rasstoyanie:');
 readln(s);          //Запрос расстояния
+writeln('Vvedite vremya:');
 readln(t);          //Запрос времени
 v:= s/t;            //Формула высчитывания скорости
 writeln(v);         //Вывод скорости на экран
