2ba0d82010d839625880a3b1c54438116496e8e6
IveLosTmyMinD <ivelostmymind8@gmail.com>
2016-11-13 20:51:09 +0300

    Update 7.4.pas

--- inf/7/7.4.pas
+++ inf/7/7.4.pas
@@ -2,11 +2,13 @@ var n, s:integer;
 begin
 s:=0;
 readln(n);
-while n > 0 do
-  begin
-    s:=s+(n mod 10);
-    n:=n div 10;
-    
-  end;
+  if n < 30000 then
+    while n > 0 do
+        begin
+          s:=s+(n mod 10);
+          n:=n div 10;
+         end
+ else
+    writeln(n, '>=30000');
 writeln(s);
 end.
