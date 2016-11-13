f80107a5a4b340062b5d6f36182e11d7ddd64050
IveLosTmyMinD <ivelostmymind8@gmail.com>
2016-11-13 18:26:17 +0300

    Create 9.7.pas

---
+++ inf/9/9.7.pas
@@ -0,0 +1,18 @@
+var a:array [1..10] of integer;
+i, n, k, s, c,b:integer;
+begin
+s:=0;
+writeln('Задайте интервал значений');
+readln(b, c);
+  for i:=1 to 10 do
+    begin
+      readln(a[i]);
+        if (a[i] >= b) and (a[i] <= c) then
+          begin
+            k:=k+1;
+            s:=s+a[i];
+          end;
+    end;
+writeln('Kol-vo ', k);
+writeln('Summa ', s);
+end.
