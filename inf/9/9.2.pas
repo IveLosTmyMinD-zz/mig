2b29da726a5fe2a2bf0f13b91b389dc86d5947a0
IveLosTmyMinD <ivelostmymind8@gmail.com>
2016-11-13 18:03:54 +0300

    Create 9.2.pas

---
+++ inf/9/9.2.pas
@@ -0,0 +1,15 @@
+var a:array [1..10] of integer;
+i, n, k, s:integer;
+begin
+  for i:=1 to 10 do
+    begin
+      readln(a[i]);
+       if a[i] < 0 then
+          begin
+            k:=k+1;
+            s:=s+a[i];
+          end;
+    end;
+writeln('Количество отрицательных элементов массива: ', k);
+writeln('Сумма отрицательных элементов массива: ',s);
+end.
