14035abec310702fcf3f3aeffbaae682d59267af
IveLosTmyMinD <ivelostmymind8@gmail.com>
2016-11-13 18:07:32 +0300

    Create 9.4.pas

---
+++ inf/9/9.4.pas
@@ -0,0 +1,14 @@
+var a:array [1..10] of integer;
+i, n, k, s:integer;
+begin
+s:=1;
+  for i:=1 to 10 do
+    begin
+      readln(a[i]);
+       if a[i] mod 2 = 0 then
+          begin
+            k:=k+1;
+          end;
+    end;
+writeln('Количество четных элементов массива: ', k);
+end.
