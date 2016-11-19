6b77528dd2be77a0bd0c9732662c556a08fa59de
IveLosTmyMinD <ivelostmymind8@gmail.com>
2016-11-19 23:58:09 +0300

    Update 7.4.pas

--- inf/7/7.4.pas
+++ inf/7/7.4.pas
@@ -1,14 +1,25 @@
-var n, s:integer;
+var
+  n, s, a, b, k, z, s1: integer;
+
+
 begin
-s:=0;
-readln(n);
-  if n < 30000 then
-    while n > 0 do
-        begin
-          s:=s+(n mod 10);
-          n:=n div 10;
-         end
- else
-    writeln(n, '>=30000');
-writeln(s);
+  s := 1;
+  k := 0;
+  writeln('Введите n:');
+  readln(n);
+  while n > 0 do
+  begin
+    z := n mod 10;
+    if z mod 2 <> 0 then
+    begin
+      s := s * z;
+      k := k + 1;
+    end;
+    n := n div 10;
+  end;
+  
+  writeln('Proizvedenie ', s);
+  writeln('Koli4estvo ', k);
+  
+  readln;
 end.
