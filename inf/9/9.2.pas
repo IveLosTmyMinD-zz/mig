1389fef0ff6eb4c0b9ad5e6de0e529fcd778a864
IveLosTmyMinD <ivelostmymind8@gmail.com>
2016-11-20 00:31:35 +0300

    Update 9.2.pas

--- inf/9/9.2.pas
+++ inf/9/9.2.pas
@@ -1,16 +1,24 @@
-var a:array [1..10] of integer;
+var a:array [1..123456789] of integer;
 i, n, k, s:integer;
+
 begin
-  for i:=1 to 10 do
-    begin
-    writeln('Vvedite ', i,' element massiva');
+writeln('Введите количество элементов массива:');
+readln(n);
+for i:=1 to n do begin 
+writeln('Введите ', i, ' элемент массива');
       readln(a[i]);
+end; 
+  for i := 1 to n do
+    
+  begin
+    
        if a[i] < 0 then
           begin
-            k:=k+1;
-            s:=s+a[i];
+            k := k + 1;
+            s := s + a[i];
           end;
     end;
 writeln('Количество отрицательных элементов массива: ', k);
-writeln('Сумма отрицательных элементов массива: ',s);
+writeln('Сумма отрицательных элементов массива: ', s);
+readln;
 end.
