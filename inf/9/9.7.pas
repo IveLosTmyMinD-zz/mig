e6b8e10102c670cc7913f54b19001607fc9f7ad3
IveLosTmyMinD <ivelostmymind8@gmail.com>
2016-11-20 00:33:56 +0300

    Update 9.7.pas

--- inf/9/9.7.pas
+++ inf/9/9.7.pas
@@ -1,19 +1,28 @@
-var a:array [1..10] of integer;
-i, n, k, s, c,b:integer;
+var
+  a: array [1..123456789] of integer;
+i, n, k, s, c, b: integer;
+
+
 begin
-s:=0;
+s := 0;
 writeln('Задайте интервал значений');
 readln(b, c);
-  for i:=1 to 10 do
-    begin
-    writeln('Vvedite ', i,' element massiva');
+ s:=1;
+writeln('Введите количество элементов массива:');
+readln(n);
+for i:=1 to n do begin 
+writeln('Введите ', i, ' элемент массива');
       readln(a[i]);
+  begin
+   
         if (a[i] >= b) and (a[i] <= c) then
           begin
-            k:=k+1;
-            s:=s+a[i];
+            k := k + 1;
+            s := s + a[i];
           end;
     end;
-writeln('Kol-vo ', k);
-writeln('Summa ', s);
+    end;
+writeln('Количество ', k);
+writeln('Сумма ', s);
+readln;
 end.
