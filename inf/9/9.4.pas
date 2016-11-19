aec91b69124932534f1ecfe50f9832210e6299c5
IveLosTmyMinD <ivelostmymind8@gmail.com>
2016-11-20 00:32:47 +0300

    Update 9.4.pas

--- inf/9/9.4.pas
+++ inf/9/9.4.pas
@@ -1,15 +1,23 @@
-var a:array [1..10] of integer;
-i, n, k, s:integer;
+var
+  a: array [1..123456789] of integer;
+i, n, k, s: integer;
+
+
 begin
 s:=1;
-  for i:=1 to 10 do
-    begin
-    writeln('Vvedite ', i,' element massiva');
+writeln('Введите количество элементов массива:');
+readln(n);
+for i:=1 to n do begin 
+writeln('Введите ', i, ' элемент массива');
       readln(a[i]);
+  begin
+    
        if a[i] mod 2 = 0 then
           begin
-            k:=k+1;
+            k := k + 1;
           end;
     end;
+    end;
 writeln('Количество четных элементов массива: ', k);
+readln;
 end.
